6f0d9a68fca35eaf73fc3ad42c472462b26883a6
Дима <@DESKTOP-TC925PR>
2022-11-20 23:36:59 +0200

    Add project files.

---
+++ 07.11/07.11.cpp
@@ -0,0 +1,142 @@
+﻿#include <windows.h>
+#include <tchar.h>
+#include <commctrl.h>
+#include <windowsX.h>
+#include "resource.h"
+
+BOOL CALLBACK DlgProc(HWND, UINT, WPARAM, LPARAM);
+
+HWND hButton, hEdit1, hEdit2, hProgress;
+HMENU hMenu;
+
+int TrueAnswer = 0, MaxAnswer = 8;
+
+TCHAR str[50];
+TCHAR percent[] = TEXT("%");
+TCHAR idok1[50];
+TCHAR idok2[50];
+TCHAR CorrectBoxAnswer1[] = TEXT("Ìîëíèÿ Ìàêóèí");
+TCHAR CorrectBoxAnswer2[] = TEXT("Òà÷êè");
+
+WPARAM iProgessPosition = 0;
+DWORD IDC_TIMER;
+
+
+int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPTSTR lpszCmdLine, int nCmdShow)
+{
+	return DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc);
+}
+
+void Cls_OnTimer(HWND hWnd, UINT id)
+{
+	SendMessage(hProgress, PBM_STEPIT, 0, 0);
+}
+
+BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_INITDIALOG: {
+		SendDlgItemMessage(hWnd, IDC_RADIO3, BM_SETCHECK, WPARAM(BST_UNCHECKED), 0);
+		SendDlgItemMessage(hWnd, IDC_RADIO5, BM_SETCHECK, WPARAM(BST_UNCHECKED), 0);
+		SendDlgItemMessage(hWnd, IDC_RADIO13, BM_SETCHECK, WPARAM(BST_UNCHECKED), 0);
+		SendDlgItemMessage(hWnd, IDC_RADIO9, BM_SETCHECK, WPARAM(BST_UNCHECKED), 0);
+		SendDlgItemMessage(hWnd, IDC_RADIO12, BM_SETCHECK, WPARAM(BST_UNCHECKED), 0);
+		SendDlgItemMessage(hWnd, IDC_RADIO17, BM_SETCHECK, WPARAM(BST_UNCHECKED), 0);
+		SendDlgItemMessage(hWnd, IDC_CHECK1, BM_SETCHECK, WPARAM(BST_UNCHECKED), 0);
+		SendDlgItemMessage(hWnd, IDC_CHECK2, BM_SETCHECK, WPARAM(BST_UNCHECKED), 0);
+
+		hButton = GetDlgItem(hWnd, IDC_BUTTON1);
+		hEdit1 = GetDlgItem(hWnd, IDC_EDIT1);
+		hEdit2 = GetDlgItem(hWnd, IDC_EDIT2);
+		hProgress = GetDlgItem(hWnd, IDC_PROGRESS1);
+		hMenu = LoadMenu(GetModuleHandle(NULL), MAKEINTRESOURCE(IDR_MENU1));
+		SetMenu(hWnd, hMenu);
+
+		SendMessage(hProgress, PBM_SETRANGE, 0, MAKELPARAM(0, 100));
+		SendMessage(hProgress, PBM_SETSTEP, 10, 0);
+		SendMessage(hProgress, PBM_SETPOS, 0, 0);
+	}
+					  return TRUE;
+
+	case WM_COMMAND: {
+		if (LOWORD(wParam) == IDC_RADIO3) {
+			SendDlgItemMessage(hWnd, IDC_RADIO3, BM_SETCHECK, WPARAM(BST_CHECKED), 0);
+			TrueAnswer++;
+		}
+		if (LOWORD(wParam) == IDC_RADIO5) {
+			SendDlgItemMessage(hWnd, IDC_RADIO5, BM_SETCHECK, WPARAM(BST_CHECKED), 0);
+			TrueAnswer++;
+		}
+		if (LOWORD(wParam) == IDC_RADIO9) {
+			SendDlgItemMessage(hWnd, IDC_RADIO9, BM_SETCHECK, WPARAM(BST_CHECKED), 0);
+			TrueAnswer++;
+		}
+		if (LOWORD(wParam) == IDC_RADIO12) {
+			SendDlgItemMessage(hWnd, IDC_RADIO12, BM_SETCHECK, WPARAM(BST_CHECKED), 0);
+			TrueAnswer++;
+		}
+		if (LOWORD(wParam) == IDC_RADIO13) {
+			SendDlgItemMessage(hWnd, IDC_RADIO13, BM_SETCHECK, WPARAM(BST_CHECKED), 0);
+			TrueAnswer++;
+		}
+		if (LOWORD(wParam) == IDC_RADIO17) {
+			SendDlgItemMessage(hWnd, IDC_RADIO17, BM_SETCHECK, WPARAM(BST_CHECKED), 0);
+			TrueAnswer++;
+		}
+		if (LOWORD(wParam) == IDC_CHECK1) {
+			SendDlgItemMessage(hWnd, IDC_CHECK1, BM_SETCHECK, WPARAM(BST_CHECKED), 0);
+			TrueAnswer++;
+		}
+		if (LOWORD(wParam) == IDC_CHECK2) {
+			SendDlgItemMessage(hWnd, IDC_CHECK2, BM_SETCHECK, WPARAM(BST_CHECKED), 0);
+			TrueAnswer++;
+		}
+		if (LOWORD(wParam) == IDC_CHECK3 || LOWORD(wParam) == IDC_CHECK4) {
+			TrueAnswer--;
+		}
+		if (LOWORD(wParam) == IDOK1) {
+			int size1 = GetWindowTextLength(hEdit1);
+			GetWindowText(hEdit1, idok1, size1 + 1);
+			if (idok1 == CorrectBoxAnswer1) {
+				TrueAnswer++;
+			}
+			else if (idok1 == NULL) {
+				TrueAnswer--;
+			}
+			else if (idok1 != CorrectBoxAnswer1) {
+				TrueAnswer--;
+			}
+		}
+		if (LOWORD(wParam) == IDOK2) {
+			int size2 = GetWindowTextLength(hEdit2);
+			GetWindowText(hEdit2, idok2, size2 + 1);
+			if (idok2 == CorrectBoxAnswer2) {
+				TrueAnswer++;
+			}
+			else if (idok2 == NULL) {
+				TrueAnswer--;
+			}
+			else if (idok2 != CorrectBoxAnswer2) {
+				TrueAnswer--;
+			}
+		}
+		if (LOWORD(wParam) == IDC_BUTTON1) {
+			TrueAnswer = (double(TrueAnswer) / MaxAnswer) * 100;
+			wsprintf(str, TEXT("Âàø ðåçóëüòàò = %d %hs"), TrueAnswer, percent);
+			SendMessage(hProgress, PBM_SETPOS, WPARAM(TrueAnswer), 0);
+			EndDialog(hWnd, 0);
+		}
+		if (LOWORD(wParam) == ID_MENU_EXIT) {
+			EndDialog(hWnd, 0);
+		}
+	}
+				   return TRUE;
+
+
+	case WM_CLOSE:
+		EndDialog(hWnd, 0);
+		return TRUE;
+	}
+	return FALSE;
+}
---
+++ 07.11/Resource.h
@@ -0,0 +1,44 @@
+﻿
+#define IDD_DIALOG1                     101
+#define IDB_BITMAP1                     103
+#define IDR_MENU1                       104
+#define IDC_RADIO1                      1001
+#define IDC_RADIO2                      1002
+#define IDC_RADIO3                      1003
+#define IDC_RADIO4                      1004
+#define IDC_RADIO5                      1005
+#define IDC_RADIO6                      1006
+#define IDC_RADIO7                      1007
+#define IDC_RADIO8                      1008
+#define IDC_RADIO9                      1009
+#define IDC_RADIO10                     1010
+#define IDC_RADIO11                     1011
+#define IDC_RADIO12                     1012
+#define IDC_RADIO13                     1013
+#define IDC_RADIO14                     1014
+#define IDC_RADIO15                     1015
+#define IDC_RADIO16                     1016
+#define IDC_RADIO17                     1017
+#define IDC_RADIO18                     1018
+#define IDC_CHECK1                      1019
+#define IDC_CHECK2                      1020
+#define IDC_CHECK3                      1021
+#define IDC_CHECK4                      1022
+#define IDC_BUTTON1                     1023
+#define IDC_EDIT1                       1024
+#define IDC_EDIT2                       1025
+#define IDOK1                           1028
+#define IDOK2                           1029
+#define IDC_PROGRESS1                   1030
+#define IDC_EDIT3                       1032
+#define IDC_SPIN2                       1033
+#define ID_MENU_EXIT                    40001
+
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_COMMAND_VALUE         40002
+#define _APS_NEXT_CONTROL_VALUE         1034
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
